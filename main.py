78b2b16907085832a1015c1bb9b3af2caab9cad8
Thomas Vincent <tlfvincent@gmail.com>
2022-08-14 19:47:26 -0400

    fix logging and format date columns

--- main.py
+++ main.py
@@ -3,17 +3,27 @@ Main
 
 This is the main entrypoint to the application
 """
-from src import extract_data
+
+import logging
+import sys
 
 import pandas as pd
 
+from src import extract_data
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+# NBA_TEAMS = [
+#     "ATL", "BOS", "CHA", "CHI", "CLE",
+#     "DAL", "DEN", "DET", "GSW", "HOU",
+#     "IND", "LAC", "LAL", "MEM", "MIA",
+#     "MIL", "MIN", "NJN", "NOH", "NYK",
+#     "OKC", "ORL", "PHI", "PHO", "POR",
+#     "SAC", "SAS", "TOR", "UTA", "WAS"
+# ]
 NBA_TEAMS = [
     "ATL", "BOS", "CHA", "CHI", "CLE",
-    "DAL", "DEN", "DET", "GSW", "HOU",
-    "IND", "LAC", "LAL", "MEM", "MIA",
-    "MIL", "MIN", "NJN", "NOH", "NYK",
-    "OKC", "ORL", "PHI", "PHO", "POR",
-    "SAC", "SAS", "TOR", "UTA", "WAS"
 ]
 
 
@@ -26,10 +36,9 @@ def main():
 
     trade_info = extract_data.trader_crawler(nba_team_pairs)
 
-    df = pd.DataFrame(
-        trade_info,
-        columns=["team1", "team2", "win_shares_sent", "win_shares_received", "trade_date"]
-    )
+    df = pd.concat(trade_info)
+    df["trade_date"] = pd.to_datetime(df["trade_date"])
+    df["trade_date"].dt.strftime('%Y-%m')
 
     df.to_csv("trade_win_shares.csv", index=False)
 
